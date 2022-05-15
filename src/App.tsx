2f9a8a866744a752ec36072afb75bf207a904c37
kk <a631953720@gmail.com>
2022-05-15 22:16:26 +0800

    chore: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.timeout = 10 * 1000;
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        // 打api錯誤時印出error
+        // eslint-disable-next-line no-console
+        console.error(error);
+        return Promise.reject(error);
+      },
+    );
+    return () => {
+      axios.interceptors.response.eject(responseInterceptor);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +28,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/constant/env.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+export const DEBUG = process.env.REACT_APP_DEBUT;
+
+export const SERVER = {
+  TBToolHost: process.env.REACT_APP_TB_TOOL_SERVER || '127.0.0.1:3000',
+};
